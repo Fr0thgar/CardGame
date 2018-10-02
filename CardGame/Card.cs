479c8af11be91424ff64196abc8df77f756055a4
Fr0thgar <jona270r@edu.easj.dk>
2018-10-02 10:42:16 +0200

    new return value

--- CardGame/Card.cs
+++ CardGame/Card.cs
@@ -19,7 +19,7 @@ namespace CardGame
 
         public override string ToString()
         {
-            return Value + ", " + Colour;
+            return $"[{Value}, {Colour}]";
         }
     }
 }
--- CardGame/Deck.cs
+++ CardGame/Deck.cs
@@ -14,15 +14,19 @@ namespace CardGame
         {
             for (int i = 0; i < 10; i++)
             {
+                //Red Cards
                 cards[i] = new Card("red", i);
                 cards[i + 9] = new Card("red", i);
 
+                //Blue Cards
                 cards[i + 19] = new Card("blue", i);
                 cards[i + 28] = new Card("blue", i);
 
+                //Yellow Cards
                 cards[i + 38] = new Card("yellow", i);
                 cards[i + 47] = new Card("yellow", i);
 
+                //Green Cards
                 cards[i + 57] = new Card("green", i);
                 cards[i + 66] = new Card("green", i);
             }
